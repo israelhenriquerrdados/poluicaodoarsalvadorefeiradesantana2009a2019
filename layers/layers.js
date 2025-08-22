e5b61c9ab9d367227d468d7b8847c9ec0f316a9d
israelhenriquerrdados <israelhenriquerr@usp.br>
2025-08-22 09:04:45 -0300

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,207 @@
+var wms_layers = [];
+
+var lyr_RMSRMFS2010_0 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2010<br />\
+    <img src="styles/legend/RMSRMFS2010_0_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2010_0_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2010_0_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2010_0_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2010_0_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2010_0.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2011_1 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2011<br />\
+    <img src="styles/legend/RMSRMFS2011_1_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2011_1_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2011_1_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2011_1_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2011_1_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2011_1.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2012_2 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2012<br />\
+    <img src="styles/legend/RMSRMFS2012_2_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2012_2_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2012_2_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2012_2_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2012_2_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2012_2.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2013_3 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2013<br />\
+    <img src="styles/legend/RMSRMFS2013_3_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2013_3_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2013_3_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2013_3_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2013_3_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2013_3.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2014_4 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2014<br />\
+    <img src="styles/legend/RMSRMFS2014_4_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2014_4_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2014_4_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2014_4_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2014_4_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2014_4.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2015_5 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2015<br />\
+    <img src="styles/legend/RMSRMFS2015_5_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2015_5_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2015_5_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2015_5_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2015_5_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2015_5.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2016_6 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2016<br />\
+    <img src="styles/legend/RMSRMFS2016_6_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2016_6_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2016_6_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2016_6_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2016_6_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2016_6.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2017_7 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2017<br />\
+    <img src="styles/legend/RMSRMFS2017_7_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2017_7_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2017_7_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2017_7_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2017_7_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2017_7.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2018_8 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2018<br />\
+    <img src="styles/legend/RMSRMFS2018_8_0.png" /> 4,2000<br />\
+    <img src="styles/legend/RMSRMFS2018_8_1.png" /> 5,4000<br />\
+    <img src="styles/legend/RMSRMFS2018_8_2.png" /> 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2018_8_3.png" /> 7,8000<br />\
+    <img src="styles/legend/RMSRMFS2018_8_4.png" /> 9,0000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2018_8.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+var lyr_RMSRMFS2019_9 = new ol.layer.Image({
+        opacity: 1,
+        
+    title: 'RMSRMFS2019<br />\
+    <img src="styles/legend/RMSRMFS2019_9_0.png" /> <= 5,0000<br />\
+    <img src="styles/legend/RMSRMFS2019_9_1.png" /> 5,0000 - 5,8000<br />\
+    <img src="styles/legend/RMSRMFS2019_9_2.png" /> 5,8000 - 6,6000<br />\
+    <img src="styles/legend/RMSRMFS2019_9_3.png" /> 6,6000 - 7,4000<br />\
+    <img src="styles/legend/RMSRMFS2019_9_4.png" /> 7,4000 - 8,2000<br />\
+    <img src="styles/legend/RMSRMFS2019_9_5.png" /> > 8,2000<br />' ,
+        
+        
+        source: new ol.source.ImageStatic({
+            url: "./layers/RMSRMFS2019_9.png",
+            attributions: ' ',
+            projection: 'EPSG:3857',
+            alwaysInRange: true,
+            imageExtent: [-4512892.156759, -1460874.771411, -4219008.701065, -1176584.779824]
+        })
+    });
+
+        var lyr_OSMStandard_10 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 0.498000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &nbsp &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+lyr_RMSRMFS2010_0.setVisible(true);lyr_RMSRMFS2011_1.setVisible(true);lyr_RMSRMFS2012_2.setVisible(true);lyr_RMSRMFS2013_3.setVisible(true);lyr_RMSRMFS2014_4.setVisible(true);lyr_RMSRMFS2015_5.setVisible(true);lyr_RMSRMFS2016_6.setVisible(true);lyr_RMSRMFS2017_7.setVisible(true);lyr_RMSRMFS2018_8.setVisible(true);lyr_RMSRMFS2019_9.setVisible(true);lyr_OSMStandard_10.setVisible(true);
+var layersList = [lyr_RMSRMFS2010_0,lyr_RMSRMFS2011_1,lyr_RMSRMFS2012_2,lyr_RMSRMFS2013_3,lyr_RMSRMFS2014_4,lyr_RMSRMFS2015_5,lyr_RMSRMFS2016_6,lyr_RMSRMFS2017_7,lyr_RMSRMFS2018_8,lyr_RMSRMFS2019_9,lyr_OSMStandard_10];
